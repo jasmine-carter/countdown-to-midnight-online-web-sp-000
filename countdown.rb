f8d132a3116e2f504ec4eaeb5ed269d13124e483
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-20 22:41:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,7 +12,7 @@ countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECOND(S)!\n"
     number= number -1
-    sleep(1)
+    sleep(1.seconds)
   end
   return "HAPPY NEW YEAR!"
 end
